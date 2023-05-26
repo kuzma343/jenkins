8eb495d519518d830c2361aea0f07647c796a0d2
kuzma343 <129772725+kuzma343@users.noreply.github.com>
2023-05-26 20:31:32 +0300

    Update qwe.sh

--- qwe.sh
+++ qwe.sh
@@ -1,13 +1,7 @@
 node {
     stage('Install Midnight Commander') {
         withCredentials([string(credentialsId: 'sudo-password', variable: 'PASSWORD')]) {
-            sh '''
-            echo $PASSWORD | sudo -S apt-get install -y mc
-            '''
+            sh "echo $PASSWORD | sudo -S apt-get install -y mc"
         }
     }
 }
-
-
-
-
